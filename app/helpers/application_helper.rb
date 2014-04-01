a91106511ef288111c5b747c697f3e4e75b1116f
p04mu <petr.murom@gmail.com>
2014-04-02 01:18:45 +0400

    full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# encoding: UTF-8
 module ApplicationHelper
+
+	def full_title(page_title)
+		base_title = "Руби Уроки"
+		if page_title.empty?
+			base_title
+		else "#{base_title} | #{page_title}"
+		end
+	end
+
 end
